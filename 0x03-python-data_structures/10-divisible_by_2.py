3dbfdbd2f26866678fe0208295ddb9d47ee435b3
samisymbian <samisymbian@gmail.com>
2023-12-04 15:13:17 -0800

    dos.0.3

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    boo=[]
-    for i in range(len(my_list)-1):
-        if my_list[i]%2==0:
-            boo.append(1)
+    new_list = []
+    for i in range(len(my_list)):
+        if my_list[i] % 2 == 0:
+            new_list.append(True)
         else:
-            boo.append(0)
-    return boo
+            new_list.append(False)
+    return new_list
